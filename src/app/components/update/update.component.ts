5656f0067b2c873c34ee6a6141ba1d2c64772ba6
Rui Wang <rui.wang.624@gmail.com>
2021-02-19 19:14:39 -0500

    finished update account page

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+  updateForm: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.updateForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(256)]],
+      email: ['', [Validators.required, Validators.email, Validators.minLength(6), Validators.maxLength(256)]],
+      oldPassword: ['', [Validators.required, Validators.min(6), Validators.max(256)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(256)]]
+    })
+  }
+
+  onSubmit() {
+    console.log('Form submitted.');
   }
 
 }
